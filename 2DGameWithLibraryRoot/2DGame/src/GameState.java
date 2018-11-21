01bfbd8ecfca41d7b1124369dc52839249b4872d
guest <kamil.topolewski@gmail.com>
2018-11-21 23:16:35 +0100

    Added a sight limit

--- 2DGameWithLibraryRoot/2DGame/src/GameState.java
+++ 2DGameWithLibraryRoot/2DGame/src/GameState.java
@@ -2,32 +2,36 @@ import java.awt.*;
 
 public class GameState extends State {
 
-    private Player player;
-    private World world;
-    private PowerUpEntity e;
+	private Player player;
+	private World world;
+	private PowerUpEntity e;
+	private Sight s;
 
-    public GameState( Handler handler ){ // constructor
-        super(handler);
-        world = new World(handler,"res1/maps/map0.txt");
-        handler.setWorld(world);
-        player = new Player( handler ,64, 64);
-        e = new PowerUpEntity(handler, 100, 100);
-    }
+	public GameState(Handler handler) { // constructor
+		super(handler);
+		world = new World(handler, "res1/maps/map0.txt");
+		handler.setWorld(world);
+		player = new Player(handler, 64, 64);
+		e = new PowerUpEntity(handler, 100, 100);
+		s = new Sight(handler, 100, 100);
+	}
 
-    public void update(){
-        world.update();
-        e.update();
-        player.update();
-        
-    }
+	public void update() {
+		world.update();
+		e.update();
+		player.update();
+		s.update();
 
-    //draws things in this state
-    public void render(Graphics g){
+	}
 
-        world.render(g);
-        e.render(g);
-        player.render(g);
-       
+	// draws things in this state
+	public void render(Graphics g) {
 
-    }
+		world.render(g);
+		e.render(g);
+		s.render(g);
+		player.render(g);
+		
+
+	}
 }
--- 2DGameWithLibraryRoot/2DGame/src/Player.java
+++ 2DGameWithLibraryRoot/2DGame/src/Player.java
@@ -6,8 +6,8 @@ public class Player extends Creature {
     public Player(Handler handler, float x, float y) {
         super(handler, x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
 
-        bounds.x = 10;
-        bounds.y = 5;
+        bounds.x = -17;
+        bounds.y = -28;
         bounds.width = 30;
         bounds.height = 50;
     }
@@ -35,9 +35,15 @@ public class Player extends Creature {
             xMove = -speed;
         }
     }
+    public float getX() {
+    	return x;
+    }
+    public float getY() {
+    	return y;
+    }
 
     public void render(Graphics g) {
-        g.drawImage(Assets.policeFront, (int) (x - handler.getGameCamera().getxOffset()), (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
+        g.drawImage(Assets.policeFront, (int) (x - 30 - handler.getGameCamera().getxOffset()), (int) (y - 32 - handler.getGameCamera().getyOffset()), width, height, null);
        
 //        g.setColor(Color.red);
 //        g.fillRect((int) (x + bounds.x - handler.getGameCamera().getxOffset()), (int) (y + bounds.y - handler.getGameCamera().getyOffset()), bounds.width, bounds.height);
