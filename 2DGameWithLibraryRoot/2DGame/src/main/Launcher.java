40b210192a9d8036f0b4a2a7bff6b4c4c20416e6
cometqq1 <43993311+cometqq1@users.noreply.github.com>
2018-11-29 13:50:59 +0100

    menu working

--- 2DGameWithLibraryRoot/2DGame/src/main/Launcher.java
+++ 2DGameWithLibraryRoot/2DGame/src/main/Launcher.java
@@ -1,9 +1,24 @@
 package main;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
 //responsible of launching the game
 public class Launcher {
 
     public static void main(String[] args){
         Game game = new Game("Maze game", 900, 900); // object game which runs Game constructor
         game.start();
+        try {
+            File sound = new File("res1/sounds//catchme.wav");
+            AudioInputStream ais = AudioSystem.getAudioInputStream(sound);
+            Clip clip = AudioSystem.getClip();
+            clip.open(ais);
+            clip.start();
+        }catch (Exception e){System.out.println(e);
+        }
+
     }
 }
--- 2DGameWithLibraryRoot/2DGame/src/states/MenuState.java
+++ 2DGameWithLibraryRoot/2DGame/src/states/MenuState.java
@@ -1,22 +1,44 @@
 package states;
 import java.awt.*;
-
 import main.Handler;
 import main.State;
 
 public class MenuState extends State {
 
-    public MenuState( Handler handler ) {
+    public MenuState(Handler handler) {
         super(handler);
-
     }
 
+    @Override
     public void update() {
+        //System.out.println(handler.getMouseManager().getMouseX() + "     " +handler.getMouseManager().getMouseY());
 
+        if(handler.getMouseManager().isLeftPressed() )
+        { State.setState(handler.getGame().gameState);}
     }
 
+    //public Rectangle playButton = new Rectangle( handler.getWidth()/2 + 120 , 150, 100 , 50);
+    public Rectangle playButton = new Rectangle( handler.getWidth()/2 + 120 , 150, 300 , 50);
+    public Rectangle settingsButton = new Rectangle( handler.getWidth()/2 + 120 , 250, 300 , 50); // for each button just add to y + 100
+
+    @Override
     public void render(Graphics g) {
 
-    }
+            Graphics2D g2d = (Graphics2D) g;
 
+            Font fnt0 = new Font("arial", Font.BOLD, 50);
+            g.setFont(fnt0);
+            g.setColor(Color.black);
+            g.drawString("MAZE GAME",handler.getWidth()/ 2, 100); // x is getWidth()
+
+            Font fnt1 = new Font("arial", Font.BOLD, 50);
+            g.setFont(fnt1);
+            g.drawString("PLAY", playButton.x + 19, playButton.y + 45);
+            g2d.draw(playButton);
+            g.drawString("SETTINGS", settingsButton.x + 19, settingsButton.y + 45);
+            g2d.draw(settingsButton);
+
+        g.setColor(Color.RED);
+        g.fillRect(handler.getMouseManager().getMouseX() , handler.getMouseManager().getMouseY(), 8, 8);
+    }
 }
